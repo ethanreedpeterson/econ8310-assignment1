744d7668e849edacabff30733ac5d738f3f1d2e5
ethanreedpeterson <ethanrpete@gmail.com>
2026-01-30 20:32:30 -0600

    Commit 05: Labeling sections of GAM code

--- assignment1.py
+++ assignment1.py
@@ -1,9 +1,9 @@
-#GAM METHOD
+# Generalized Additive Model route!
 import numpy as np
 import pandas as pd
 from pygam import LinearGAM, s, f
 
-# data
+# data loading
 train_url = "https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv"
 test_url  = "https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_test.csv"
 
@@ -16,6 +16,7 @@ y_train = train["trips"].values
 
 X_test = test[["hour", "day", "month"]].values
 
+# actual model
 model = LinearGAM(
     s(0, n_splines=24) +
     s(1, n_splines=31) +
@@ -25,7 +26,7 @@ model = LinearGAM(
 modelFit = model.fit(X_train, y_train)
 pred = modelFit.predict(X_test)
 
+# output of prediction
 forecast_df = test.copy()
 forecast_df["predicted_trips"] = pred
-
 forecast_df.head()
